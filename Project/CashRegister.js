4c9f6d6fa911aec89e06fbff7770a8582d8596d3
mahme001 <minhaz.ahmed@student.hs-rm.de>
2023-05-27 00:33:56 +0200

    Initial commit

--- Project/CashRegister.js
+++ Project/CashRegister.js
@@ -1,16 +1,95 @@
 function checkCashRegister(price, cash, cid) {
-    let change = price - cash;
-    let status = "";
+    let change = (cash - price) * 100;
+    let copyCid = [...cid];
 
-    cid.reverse();
-    
-    for(let i = 0; i<cid.length; i++) {
-        console.log(cid[i]);
+    const money = {
+        "PENNY": 1,
+        "NICKEL": 5,
+        "DIME": 10,
+        "QUARTER": 25,
+        "ONE": 100,
+        "FIVE": 500,
+        "TEN": 1000,
+        "TWENTY": 2000,
+        "ONE HUNDRED": 10000
     }
-    return change;
+
+    let status = "";
+    let count = 0;
+    const arr = [];
+    let res;
+    let key;
+    let value;
+
+    copyCid.reverse();
+
+    copyCid.filter(elem => elem[1] != 0).forEach(element => {
+        key = element[0];
+        value = element[1] * 100;
+
+
+        console.log(element)
+        // console.log("key " + key);
+        // console.log("value " + value);
+        // console.log();
+
+        console.log("money " + money[key]);
+        console.log("key " + change);
+
+
+        if (change >= money[key]) {
+            console.log("gepusht")
+            arr.push([key, element[1]]);
+            while (change >= 0 && value > 0 && change >= money[key]) {
+                change = change - money[key];
+                console.log("change " + change);
+                // console.log(money[key]);
+                // console.log();
+                value--;
+            }
+        }
+
+        count++;
+    });
+
+    // console.log(change);
+    console.log("Mein arr:");
+    console.log(arr);
+    console.log();
+
+    if (change == 0 && count === 1) {
+        status = "CLOSED"
+        res = {
+            status: status,
+            changed: cid
+        };
+    } else if (change == 0 && count > 1) {
+        arr.map(elem => elem[1]/100);
+        status = "OPEN"
+        res = {
+            status: status,
+            changed: arr
+        };
+    } else {
+        status = "INSUFFICIENT_FUNDS";
+        res = {
+            status: status,
+            change: []
+        };
+    }
+
+    console.log()
+    console.log("===========")
+    console.log()
+    console.log("FINISH")
+    console.log()
+    console.log(res);
+
+    return res;
 }
 
-checkCashRegister(19.5, 20, [
+
+checkCashRegister(3.26, 100, [
     ["PENNY", 1.01],
     ["NICKEL", 2.05],
     ["DIME", 3.1],
@@ -20,50 +99,5 @@ checkCashRegister(19.5, 20, [
     ["TEN", 20],
     ["TWENTY", 60],
     ["ONE HUNDRED", 100]
-]);
-
-// {status: "OPEN", change: [
-// ["QUARTER", 0.5]
-// ]}
-
-console.log();
-
-checkCashRegister(19.5, 20, [
-    ["PENNY", 0.5],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 0],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
 ])
 
-// {status: "CLOSED", change: [
-//     ["PENNY", 0.5], 
-//     ["NICKEL", 0], 
-//     ["DIME", 0], 
-//     ["QUARTER", 0], 
-//     ["ONE", 0], 
-//     ["FIVE", 0], 
-//     ["TEN", 0], 
-//     ["TWENTY", 0], 
-//     ["ONE HUNDRED", 0]
-// ]}
-
-console.log();
-
-checkCashRegister(19.5, 20, [
-    ["PENNY", 0.01],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 0],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
-])
-
-// {status: "INSUFFICIENT_FUNDS", change: []}
